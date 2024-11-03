167eeb0fdacb335f81ff43860609e4a93d2cead9
Eetu <99178022+E3TU@users.noreply.github.com>
2024-11-03 13:50:19 +0200

    Initialized csharp console app

---
+++ TodoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TodoApp
+{
+    class TodoApp
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            
+            Console.WriteLine("Todo App");
+            Console.WriteLine("------------");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. Mark Task AsDone");
+            Console.WriteLine("3. Remove Task");
+            Console.WriteLine("4. Exit");
+            
+            option = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
+
