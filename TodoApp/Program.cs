fa03b54fd90a2b7fb8c6d91961b0e0270035e08e
Eetu <99178022+E3TU@users.noreply.github.com>
2024-11-03 22:52:47 +0200

    Methods for addtask and viewtask

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -2,54 +2,81 @@
 
 namespace TodoApp
 {
-    public class Task
+    public class TodoItem
     {
         public int Id { get; set; }
         public bool IsDone { get; set; }
-        public string Description { get; set; }
+        public string Task { get; set; }
 
-        public Task(int id, string description)
+        public TodoItem(int id, string task)
         {
             Id = id;
-            Description = description;
+            Task = task;
             IsDone = false;
         }
     }
     class TodoApp
     {
+        static List<TodoItem> tasks = new List<TodoItem>();
+        static int currentId = 1;
         static void Main(string[] args)
         {
+            
             int option;
-            
-            Console.WriteLine("Todo App");
-            Console.WriteLine("------------");
-            Console.WriteLine("1. Add Task");
-            Console.WriteLine("2. View Tasks");
-            Console.WriteLine("3. Mark Task AsDone");
-            Console.WriteLine("4. Remove Task");
-            Console.WriteLine("5. Exit");
-            Console.Write("Enter your choice: ");
-            
-            option = Convert.ToInt32(Console.ReadLine());
-            
-            
-            switch (option)
+
+            do
             {
-                case 1:
-                    Console.Write("Enter Task Description: ");
-                    break;
-                case 2:
-                    Console.WriteLine("Which task you want to mark as done:");
-                    break;
-                case 3:
-                    Console.WriteLine("Which task you want to remove:");
-                    break;
-                case 4:
-                    Console.WriteLine("Do you want to exit?");
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice");
-                    break;
+                Console.WriteLine("Todo App");
+                Console.WriteLine("------------");
+                Console.WriteLine("1. Add Task");
+                Console.WriteLine("2. View Tasks");
+                Console.WriteLine("3. Mark Task AsDone");
+                Console.WriteLine("4. Remove Task");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option: ");
+            
+                option = Convert.ToInt32(Console.ReadLine());
+                
+                switch (option)
+                {
+                    case 1:
+                        AddTask();
+                        break;
+                    case 2:
+                        ViewTasks();
+                        break;
+                    case 3:
+                        Console.WriteLine("Which task do you want to mark as done?");
+                        break;
+                    case 4:
+                        Console.WriteLine("Which task do you want to remove?");
+                        break;
+                    case 5:
+                        Console.WriteLine("Exited successfully!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            } while (option != 5);
+        }
+
+        static void AddTask()
+        {
+            Console.Write("Enter task: ");
+            string task = Console.ReadLine() ?? "default value";
+            if (string.IsNullOrEmpty(task))
+            {
+                Console.WriteLine("Task cannot be empty");
+            }
+            tasks.Add(new TodoItem(currentId++, task));
+        }
+
+        static void ViewTasks()
+        {
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tasks[i].Task}");
             }
         }
     }
