6d1ca89d4ba3e2b267f405c30f20c41603e9a7bc
Eetu <99178022+E3TU@users.noreply.github.com>
2024-11-03 15:39:09 +0200

    Switch case logic

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -11,12 +11,33 @@ namespace TodoApp
             Console.WriteLine("Todo App");
             Console.WriteLine("------------");
             Console.WriteLine("1. Add Task");
-            Console.WriteLine("2. Mark Task AsDone");
-            Console.WriteLine("3. Remove Task");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("2. View Tasks");
+            Console.WriteLine("3. Mark Task AsDone");
+            Console.WriteLine("4. Remove Task");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
             
             option = Convert.ToInt32(Console.ReadLine());
             
+            
+            switch (option)
+            {
+                case 1:
+                    Console.Write("Enter Task Description: ");
+                    break;
+                case 2:
+                    Console.WriteLine("Which task you want to mark as done:");
+                    break;
+                case 3:
+                    Console.WriteLine("Which task you want to remove:");
+                    break;
+                case 4:
+                    Console.WriteLine("Do you want to exit?");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
         }
     }
 }
